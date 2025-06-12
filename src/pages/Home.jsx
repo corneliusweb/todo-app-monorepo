36760b14a758e247d45014a20b08350aee0c4bfb
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-06-12 17:01:49 +0100

    Add link to /test-error on Home page for ErrorBoundary testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,10 @@ const Home = () => {
       <h1>Welcome to Todo App</h1>
       <nav>
         <Link to="/todos" className="nav-link">View All Todos</Link>
+        <br />
+        <Link to="/test-error" className="nav-link" style={{ marginTop: 10, background: '#ff4d4f' }}>
+          Test Error Boundary
+        </Link>
       </nav>
     </div>
   );
