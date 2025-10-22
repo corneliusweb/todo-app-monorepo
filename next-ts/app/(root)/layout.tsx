1465e259d4280425f760af6ad604832f56650959
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-10-22 09:07:08 +0100

    feat(todos-page): implement todos pagination, filter and search features

--- next-ts/app/(root)/layout.tsx
+++ next-ts/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return <main>{children}</main>;
+	return <main className='px-6 sm:px-20'>{children}</main>;
 }
---
+++ next-ts/app/types/index.ts
@@ -0,0 +1,6 @@
+export interface ITodo {
+	userId: number;
+	id: number;
+	todo: string;
+	completed: boolean;
+}
